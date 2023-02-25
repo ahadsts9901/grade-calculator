410ae5005622225ff365505f190c5b09997e2f75
ahadsts9901 <ahadsts990@gmail.com>
2023-02-25 10:55:00 -0800

    grade calculator done

---
+++ main.mjs
@@ -0,0 +1,57 @@
+let percentage = prompt('Enter Your Percentage');
+
+if (percentage>100){
+    alert ('Invalid Percentage')
+
+    console.log ('Invalid Percentage')
+}
+
+else if (percentage>=80) 
+{
+    alert ('Your Grade Is A+')
+
+    console.log ('Your Grade Is A+')
+}
+
+else if (percentage>=70) 
+{
+    alert ('Your Grade Is A')
+
+    console.log ('Your Grade Is A')
+}
+
+else if (percentage>=60) {
+    alert ('Your Grade Is B')
+
+    console.log ('Your Grade Is B')
+}
+
+else if (percentage>=50) {
+    alert ('Your Grade Is C')
+
+    console.log ('Your Grade Is C')
+}
+
+else if (percentage>=40) {
+    alert ('Your Grade Is D')
+
+    console.log ('Your Grade Is D')
+}
+
+else if (percentage>=33) {
+    alert ('Your Grade Is E')
+
+    console.log ('Your Grade Is E')
+}
+
+else if (percentage>=0) {
+    alert ('You Are Failed')
+
+    console.log ('You Are Failed')
+}
+
+else {
+    alert ('Invalid Percentage')
+
+    console.log ('Invalid Percentage')
+}
